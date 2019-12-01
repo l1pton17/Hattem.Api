fb0f85207433218c798900ab8aafc4efc81c5ab8
Artur Sitdikov <arthur.sitdikov@inn.ru>
2019-12-02 00:07:20 +0300

    tests for errorextensions

---
+++ tests/Hattem.Api.Tests/ErrorExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using Hattem.Api.Extensions;
+using Hattem.Api.Tests.Framework;
+using Hattem.Api.Tests.Framework.Comparers;
+using Hattem.Api.Tests.Framework.Errors;
+using Xunit;
+
+namespace Hattem.Api.Tests
+{
+    [CategoryTrait("ErrorExtensions")]
+    public sealed class ErrorExtensionsTests
+    {
+        [Fact(DisplayName = "Should wrap error to response")]
+        public void ToResponse()
+        {
+            var response = TestError
+                .Default
+                .ToResponse(To<int>.Type);
+
+            Assert.True(response.HasErrors);
+            Assert.False(response.IsOk);
+            Assert.Equal(TestError.Default, response.Error, ErrorComparer.Default);
+            Assert.Null(response.StatusCode);
+        }
+
+        [Fact(DisplayName = "Should wrap error to response with status code")]
+        public void ToResponse_WithStatusCode()
+        {
+            const int expected = 203;
+
+            var response = TestError
+                .Default
+                .ToResponse(expected, To<int>.Type);
+
+            Assert.True(response.HasErrors);
+            Assert.False(response.IsOk);
+            Assert.Equal(TestError.Default, response.Error, ErrorComparer.Default);
+            Assert.Equal(expected, response.StatusCode);
+        }
+    }
+}
