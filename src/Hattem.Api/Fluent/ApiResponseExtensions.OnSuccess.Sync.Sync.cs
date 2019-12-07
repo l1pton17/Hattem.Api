5fcf831a7d917bce4565c9410f1833570dbe57c1
Artur <sitdikov.arthur@gmail.com>
2019-12-07 23:35:14 +0300

    onsuccess

---
+++ src/Hattem.Api/Fluent/ApiResponseExtensions.OnSuccess.Sync.Sync.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hattem.Api.Fluent
+{
+    partial class ApiResponseExtensions
+    {
+        public static ApiResponse<T> OnSuccess<T>(
+            this ApiResponse<T> source,
+            Action<T> onSuccess)
+        {
+            if (source.IsOk)
+            {
+                onSuccess(source.Data);
+            }
+
+            return source;
+        }
+    }
+}
