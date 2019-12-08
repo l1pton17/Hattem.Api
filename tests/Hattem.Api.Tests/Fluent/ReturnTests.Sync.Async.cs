3c198c7f566541fa4f6f9dd3e5eb7ae948daf8b0
Artur Sitdikov <arthur.sitdikov@inn.ru>
2019-12-09 02:32:52 +0300

    tuple return

---
+++ tests/Hattem.Api.Tests/Fluent/ReturnTests.Sync.Async.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Hattem.Api.Fluent;
+using Hattem.Api.Tests.Framework.Errors;
+using Xunit;
+
+namespace Hattem.Api.Tests.Fluent
+{
+    partial class ReturnTests
+    {
+        [Fact(DisplayName = "(Sync, Async value factory) Should return value if response successful")]
+        public async Task Sync_AsyncValueFactory_Successful_ReturnsValue()
+        {
+            const string expected = "test";
+
+            var response = await ApiResponse
+                .Ok(1)
+                .Return(_ => Task.FromResult(expected));
+
+            Assert.True(response.IsOk);
+            Assert.False(response.HasErrors);
+            Assert.Equal(expected, response.Data);
+        }
+
+        [Fact(DisplayName = "(Sync, Async value factory) Should return error if response has error")]
+        public async Task Sync_AsyncValueFactory_HasErrors_ReturnsValue()
+        {
+            const string expected = "test";
+
+            var response = await ApiResponse
+                .Error<int>(TestError.Default)
+                .Return(_ => Task.FromResult(expected));
+
+            Assert.False(response.IsOk);
+            Assert.Null(response.Data);
+        }
+    }
+}
---
+++ tests/Hattem.Api.Tests/Framework/ISyncReturnProvider.cs
@@ -0,0 +1,38 @@
+﻿namespace Hattem.Api.Tests.Framework
+{
+    public interface ISyncReturnProvider<in TInput, out TOutput>
+    {
+        TOutput Execute(TInput input);
+    }
+
+    public interface ISyncReturnProvider<in TInput1, in TInput2, out TOutput>
+    {
+        TOutput Execute(TInput1 input1, TInput2 input2);
+    }
+
+    public interface ISyncReturnProvider<in TInput1, in TInput2, in TInput3, out TOutput>
+    {
+        TOutput Execute(TInput1 input1, TInput2 input2, TInput3 input3);
+    }
+
+    public interface ISyncReturnProvider<in TInput1, in TInput2, in TInput3, in TInput4, out TOutput>
+    {
+        TOutput Execute(
+            TInput1 input1,
+            TInput2 input2,
+            TInput3 input3,
+            TInput4 input4
+        );
+    }
+
+    public interface ISyncReturnProvider<in TInput1, in TInput2, in TInput3, in TInput4, in TInput5, out TOutput>
+    {
+        TOutput Execute(
+            TInput1 input1,
+            TInput2 input2,
+            TInput3 input3,
+            TInput4 input4,
+            TInput5 input5
+        );
+    }
+}
