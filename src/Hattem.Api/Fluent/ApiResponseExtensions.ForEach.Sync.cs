3e7f169aec43ed725594b90b085ca10f0017db33
Artur Sitdikov <arthur.sitdikov@inn.ru>
2019-12-10 01:01:10 +0300

    add ForEach

---
+++ src/Hattem.Api/Fluent/ApiResponseExtensions.ForEach.Sync.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hattem.Api.Fluent
+{
+    partial class ApiResponseExtensions
+    {
+        /// <summary>
+        /// Execute <paramref name="action"/> for each item in <paramref name="source"/> consistently
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static ApiResponse<Unit> ForEach<T>(
+            this IEnumerable<T> source,
+            Func<T, ApiResponse<Unit>> action)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            foreach (var item in source)
+            {
+                var response = action(item);
+
+                if (response.HasErrors)
+                {
+                    return response;
+                }
+            }
+
+            return ApiResponse.Ok();
+        }
+
+        /// <summary>
+        /// Execute <paramref name="action"/> for each item in <paramref name="source"/> consistently
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static ApiResponse<Unit> ForEach<T>(
+            this IEnumerable<T> source,
+            Func<T, int, ApiResponse<Unit>> action)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            var index = 0;
+
+            foreach (var item in source)
+            {
+                var response = action(item, index++);
+
+                if (response.HasErrors)
+                {
+                    return response;
+                }
+            }
+
+            return ApiResponse.Ok();
+        }
+    }
+}
