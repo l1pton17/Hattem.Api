e8b9af105fe6170f0cf257a1b58caeeea6f71f71
Artur <sitdikov.arthur@gmail.com>
2019-12-14 20:34:36 +0300

    t'

---
+++ src/Hattem.Api/Fluent/ApiResponseExtensions.ForEach.Async.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Hattem.Api.Fluent
+{
+    partial class ApiResponseExtensions
+    {
+        /// <summary>
+        /// Execute consistently <paramref name="action"/> for each item in <paramref name="source"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static async Task<ApiResponse<Unit>> ForEach<T>(
+            this IEnumerable<T> source,
+            Func<T, Task<ApiResponse<Unit>>> action
+        )
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            foreach (var item in source)
+            {
+                var response = await action(item).ConfigureAwait(false);
+
+                if (response.HasErrors)
+                {
+                    return response;
+                }
+            }
+
+            return ApiResponse.Ok();
+        }
+    }
+}
---
+++ src/Hattem.Api/Fluent/ApiResponseExtensions.ForEach.Sync.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hattem.Api.Fluent
+{
+    partial class ApiResponseExtensions
+    {
+        /// <summary>
+        /// Execute consistently <paramref name="action"/> for each item in <paramref name="source"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static ApiResponse<Unit> ForEach<T>(
+            this IEnumerable<T> source,
+            Func<T, ApiResponse<Unit>> action
+        )
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            foreach (var item in source)
+            {
+                var response = action(item);
+
+                if (response.HasErrors)
+                {
+                    return response;
+                }
+            }
+
+            return ApiResponse.Ok();
+        }
+    }
+}
