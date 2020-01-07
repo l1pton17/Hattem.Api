c84eb6843a41393dbc490285d0dc56ba762018cc
Artur Sitdikov <arthur.sitdikov@inn.ru>
2020-01-08 00:18:57 +0300

    publish nuget package

--- src/Hattem.Api.NewtonsoftJson/ApiResponseJsonResult.cs
+++ src/Hattem.Api.NewtonsoftJson/ApiResponseJsonResult.cs
@@ -5,6 +5,14 @@ using Newtonsoft.Json.Serialization;
 
 namespace Hattem.Api.NewtonsoftJson
 {
+    public static class ApiResponseJsonResult
+    {
+        public static ApiResponseJsonResult<T> Create<T>(ApiResponse<T> value)
+        {
+            return new ApiResponseJsonResult<T>(value);
+        }
+    }
+
     public sealed class ApiResponseJsonResult<T> : JsonResult
     {
         // ReSharper disable once StaticMemberInGenericType
